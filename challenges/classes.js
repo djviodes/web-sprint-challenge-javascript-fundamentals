0b30180eeb8836f28452876997bf971adee8a353
djviodes <djviodes26@gmail.com>
2020-09-18 10:18:28 -0700

    Answered questions in README.md

--- challenges/classes.js
+++ challenges/classes.js
@@ -16,24 +16,42 @@
 
 //   const cuboid = new CuboidMaker(4, 5, 5);
 
-//   class CuboidMaker{
-//       constructor(length, width, height){
-//         this.length = length;
-//         this.width = width;
-//         this.height = height;
-//       }
-//       volume(){
-//         return this.length * this.width * this.height;
-//       }
-//       surfaceArea(){
-//         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-//       }
-//   }
+  class ClassCuboidMaker{
+      constructor(length, width, height){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+      }
+      volume(){
+        return this.length * this.width * this.height;
+      }
+      surfaceArea(){
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+      }
+  }
 
-//   const cuboid = new CuboidMaker(4, 5, 5);
+  const classCuboid = new ClassCuboidMaker(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker{
+    constructor(object){
+        super(object);
+    }
+    cubeVolume(){
+        return this.length * this.width * this.height;
+    }
+    cubeSurfaceArea(){
+        return 6 * (this.length * this.width);
+    }
+}
+
+const cubeOne = new CubeMaker(4, 4, 4);
+
+console.log(cubeOne.cubeVolume());
+console.log(cubeOne.cubeSurfaceArea());
